a0c953b30a3a4fe4f773dd03b0e7f9122336505a
Chen Cong <1578143291@qq.com>
2020-03-10 14:11:07 +0000

    用户列表和分页

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,8 +12,14 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store','index']
         ]);
+    }
+
+    public function index()
+    {
+         $users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 
     //用户注册表单页
